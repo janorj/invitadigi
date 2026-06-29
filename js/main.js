dc9fae2d8c5f0e8f22f9764b0f253523a9494870
janorj <alejandro.raihuanque@gmail.com>
2026-06-29 14:29:52 -0400

    otro stylr

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ form.addEventListener('submit', function(e) {
         return;
     }
 
-    const mensaje = `🦋 *Confirmación de asistencia* 🦋%0A%0A` +
+    const mensaje = `🌿 *Confirmación de asistencia* 🌿%0A%0A` +
         `👤 *Nombre:* ${nombre}%0A` +
         `📱 *Teléfono:* ${telefono || 'No especificado'}%0A` +
         `👥 *Acompañantes:* ${invitados}%0A` +
